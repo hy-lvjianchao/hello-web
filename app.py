7e4e2d9fdad0a3ea39a91a1b6556aefeaaa278b6
hy-lvjianchao <lvjc@hyron.com>
2026-09-09 11:11:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ class Handler(BaseHTTPRequestHandler):
                 "ecs-test.txt"
             )
             test_content = (
-                "Hello from ECS Fargate1!\n"
+                "Hello from ECS Fargate2!\n"
                 f"Time: {datetime.now().isoformat()}\n"
             )
             try:
